33a0e812e66fc8e5417635622adc39e6fee1b7ba
Janus Developer <dev@retrievo.net>
2023-04-06 20:25:23 -0400

    added react-query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,42 @@
-import { useState, useEffect } from 'react';
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { useQuery } from '@tanstack/react-query';
 import './App.css';
 
 function App() {
-    const [books, setBooks] = useState([]);
-
     const client = new ApolloClient({
         uri: 'http://localhost:4000/graphql',
         cache: new InMemoryCache(),
     });
 
-    useEffect(function () {
-        client
-            .query({
-                query: gql`
-                    query {
-                        books {
-                            title
-                            author
+    const {
+        isLoading,
+        isError,
+        data: books,
+        error,
+    } = useQuery({
+        queryKey: ['books'],
+        queryFn: () =>
+            client
+                .query({
+                    query: gql`
+                        query {
+                            books {
+                                title
+                                author
+                            }
                         }
-                    }
-                `,
-            })
-            .then((result) => setBooks(result.data.books));
-    }, []);
+                    `,
+                })
+                .then((result) => result.data.books),
+    });
+
+    if (isLoading) return <h1>Loading...</h1>;
+    if (isError)
+        return error instanceof Error ? (
+            <h1>{error.message}</h1>
+        ) : (
+            <h1>Something went wrong</h1>
+        );
 
     return (
         <ul className="App">
